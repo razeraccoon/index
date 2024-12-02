806ca5a9c6cce0815817010e9dbd2d2786dde971
razeraccoon <maryreyshel@gmail.com>
2024-12-02 10:26:30 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!--Contact page: feedback, suggestions, or inquiries about the page/organization. Information accepted: Name, Contact No., email.
+-->
+
+<html>
+<head>
+</head>
+<body>
+    <div class="condiv1">
+        <h2 class="conh2_1">Any feedback, suggestions, or inquiries?</h2>
+        <p></p>
+    </div>
+    <div class="condiv2">
+        <h2 class="conh2_2">Contact Us</h2>
+        
+    </div>    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="styles.css">
+        <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet'>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="navbar">
+                <ul>
+                    <li><a class="active" href="index.php">Home</a></li>
+                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="developers.php">Developers</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
